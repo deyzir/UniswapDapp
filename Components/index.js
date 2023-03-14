e275117a06453bb38de61791d59cee5d2d283eba
deyzir <zasa333333@gmail.com>
2023-03-14 22:26:38 +0300

    first upload

---
+++ Components/index.js
@@ -0,0 +1,8 @@
+import HeroSection from "./HeroSection/HeroSection";
+import Model from "./Model/Model";
+import Navbar from "./NavBar/NavBar";
+import Toggle from "./Toggle/Toggle";
+import SearchToken from "./SearchToken/SearchToken";
+import TokenList from "./TokenList/TokenList";
+
+export{HeroSection,Model,Navbar,SearchToken,Toggle,TokenList}
